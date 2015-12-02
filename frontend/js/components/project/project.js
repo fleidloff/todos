a80d3d3da0118a1ccbc71d889673b326a2b84e4e
Frederik Leidloff <fleidloff@team.mobile.de>
2015-12-02 09:52:09 +0100

    added breakpoints for layout

--- frontend/js/components/project/project.js
+++ frontend/js/components/project/project.js
@@ -7,13 +7,13 @@ import ItemActions from "./itemActions";
 export default React.createClass({
     render() {
         return <div className="pure-g project">
-            <div className="pure-u-1-2">
+            <div className="pure-u-md-2-5 pure-u-lg-1-4 pure-u-1-2">
                 <ItemActions app={this.props.app} />
                 <Sticky>
                     <Items app={this.props.app} />
                 </Sticky>
             </div>
-            <div className="pure-u-1-2">
+            <div className="pure-u-md-3-5 pure-u-lg-3-4 pure-u-1-2">
                 <Sticky>
                     <Item app={this.props.app} />
                 </Sticky>
