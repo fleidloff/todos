5de4bdb7b9d52e5c92e3675028ff3199b0100a74
Frederik Leidloff <kontakt@frederik-leidloff.de>
2015-12-04 01:10:18 +0100

    passport integration

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,14 @@ restify.defaults({
     middleware: function(req, res, next) {
         // console.log(req.body);
         // passport.authenticate('local')
+        // passport.authenticate('local', function(err, user, info) {
+        //     if (err) { return next(err); }
+        //     if (!user) { return res.redirect('/login'); }
+        //     req.logIn(user, function(err) {
+        //         if (err) { return next(err); }
+        //         return res.redirect('/users/' + user.username);
+        //     });
+        // })(req, res, next);
         next();
     },
     onError: function (err, req, res, next) {
