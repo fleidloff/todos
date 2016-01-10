f699d6bfec48256a23f37aa07a9878c03fbdff7e
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-01-10 03:19:16 +0100

    double click item to edit

--- frontend/js/components/project/item.js
+++ frontend/js/components/project/item.js
@@ -4,6 +4,9 @@ import ConfirmLink from "../shared/confirmLink";
 import Checkbox from "../shared/checkbox";
 import Icon from "react-fontawesome";
 
+let t;
+let recentlyClicked = false;
+
 export default React.createClass({
     confirmDelete() {
         return <ConfirmLink onConfirm={this.props.app.onTrigger("delete:item-detail", this.props.data.id)} className="delete contextLink" text={<Icon name="trash-o" title="delete item" />} confirmText={<Icon name="trash" title="delete item" />} />;
@@ -11,10 +14,19 @@ export default React.createClass({
     edit() {
         return <Icon className="edit contextLink" name="edit" title="edit" onClick={this.props.app.onTrigger("select:item", this.props.data.id, true)} />;  
     },
+    select() {
+        clearTimeout(t);
+        t = setTimeout(() => {
+            recentlyClicked = false;
+        }, 500);
+        
+        this.props.app.trigger("select:item", this.props.data.id, recentlyClicked);
+        recentlyClicked = true;
+    },
     render() {
         const {title, description, active, sort, checked, id} = this.props.data;
         const style = {"borderLeft": `4px solid ${keyToColor(title)}`};
-        return <div onClick={this.props.app.onTrigger("select:item", id)} style={style} className={"item" + (active ? " active" : "") + (checked ? " checked" : "")}>
+        return <div onClick={this.select} style={style} className={"item" + (active ? " active" : "") + (checked ? " checked" : "")}>
             <Checkbox onClick={this.props.app.onTrigger("check:item", id, !checked)} checked={checked} />
             {this.confirmDelete()}
             {this.edit()}
