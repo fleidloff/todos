18cfd3a7fa8892600309d6599e5c165e498a56f6
Frederik Leidloff <fleidloff@team.mobile.de>
2016-01-13 10:02:58 +0100

    disable too dumb autocompletion

--- frontend/js/app/textareaKeyHandler.js
+++ frontend/js/app/textareaKeyHandler.js
@@ -14,24 +14,6 @@ export default function onKeyDown(what) {
             }, 0);
         }
 
-        if (e.keyCode === kc.cbOpen) {
-            e.preventDefault();
-            data[what] = data[what].substring(0, target.selectionStart) + "{}" + data[what].substring(target.selectionEnd);
-            const pos = target.selectionStart;
-            setTimeout(() => {
-                target.selectionStart = target.selectionEnd = pos + 1; 
-            }, 0);
-        }
-
-        if (e.keyCode === kc.bOpen) {
-            e.preventDefault();
-            data[what] = data[what].substring(0, target.selectionStart) + "()" + data[what].substring(target.selectionEnd);
-            const pos = target.selectionStart;
-            setTimeout(() => {
-                target.selectionStart = target.selectionEnd = pos + 1; 
-            }, 0);
-        }
-
         if (e.keyCode === kc.s && e.ctrlKey) {
             e.preventDefault();
             this.save();
@@ -42,13 +24,13 @@ export default function onKeyDown(what) {
             if (lines.length > 0) {
                 if (lines[lines.length - 1].indexOf("* ") === 0) {
                     e.preventDefault();
-                    data[what] = data[what].substring(0, target.selectionStart) + "\n* " + data[what].substring(target.selectionEnd)    
+                    data[what] = data[what].substring(0, target.selectionStart) + "\n* " + data[what].substring(target.selectionEnd)
                     const pos = target.selectionStart;
                     setTimeout(() => {
                         target.selectionStart = target.selectionEnd = pos + 3;
                     }, 0);
                 };
-            } 
+            }
         }
 
         this.setState({data});
