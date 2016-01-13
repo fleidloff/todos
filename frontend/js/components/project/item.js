6f053d4300b8b439b80f497cf2a5fc39f1ca82eb
Frederik Leidloff <fleidloff@team.mobile.de>
2016-01-13 12:11:45 +0100

    remove edit icon

--- frontend/js/components/project/item.js
+++ frontend/js/components/project/item.js
@@ -11,15 +11,12 @@ export default React.createClass({
     confirmDelete() {
         return <ConfirmLink onConfirm={this.props.app.onTrigger("delete:item-detail", this.props.data.id)} className="delete contextLink" text={<Icon name="trash-o" title="delete item" />} confirmText={<Icon name="trash" title="delete item" />} />;
     },
-    edit() {
-        return <Icon className="edit contextLink" name="edit" title="edit" onClick={this.props.app.onTrigger("select:item", this.props.data.id, true)} />;  
-    },
     select() {
         clearTimeout(t);
         t = setTimeout(() => {
             recentlyClicked = false;
         }, 500);
-        
+
         this.props.app.trigger("select:item", this.props.data.id, recentlyClicked);
         recentlyClicked = true;
     },
@@ -29,7 +26,6 @@ export default React.createClass({
         return <div onClick={this.select} style={style} className={"item" + (active ? " active" : "") + (checked ? " checked" : "")}>
             <Checkbox onClick={this.props.app.onTrigger("check:item", id, !checked)} checked={checked} />
             {this.confirmDelete()}
-            {this.edit()}
             <div className="title">{title}</div>
             <div className="arrow arrow-right"></div>
         </div>;
